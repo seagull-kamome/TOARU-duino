0e0afb343a1abf0ef3ca0d84fc577894ce2d9bf8
Hiroki Hattori <seagull.kamome@gmail.com>
2011-06-27 20:38:07 +0900

    コードを整理

--- libraries/SerialBus/SerialBus.h
+++ libraries/SerialBus/SerialBus.h
@@ -5,7 +5,31 @@
 #  define SerialBUS_H
 
 #  include <WProgram.h>
-#  include <avr/io.h>
+
+
+struct MSBFirst;
+struct LSBFirst
+{
+public:
+	enum { mask = 0x01 };
+	static inline uint8_t shift(uint8_t x) { return x >> 1; }
+
+	typedef MSBFirst Reverse;
+};
+
+struct MSBFirst
+{
+public:
+	enum { mask = 0x80 };
+	static inline uint8_t shift(uint8_t x) { return x << 1; }
+	
+	typedef LSBFirst Reverse;
+};
+
+	
+struct InputFirst { public: enum { cpha = 0x00 }; };
+struct OutputFirst { public: enum { cpha = 0x01 }; };
+
 
 
 
@@ -14,42 +38,14 @@ template <int clk, int in, int out>
 class SerialBUS
 {
 protected:
-	inline SerialBUS() {}
-	inline ~SerialBUS() {}
+	SerialBUS() {}
+	~SerialBUS() {}
 
 public:
-	enum { pinCLK = clk };
 
+	enum { pinCLK = clk, pinIn = in, pinOut = out };
 
-	struct MSBFirst;
-	struct LSBFirst
-	{
-	public:
-		enum { mask = 0x01 };
-		static inline uint8_t shift(uint8_t x) { return x >> 1; }
-	
-		typedef MSBFirst Reverse;
-	};
-	
-	struct MSBFirst
-	{
-	public:
-		enum { mask = 0x80 };
-		static inline uint8_t shift(uint8_t x) { return x << 1; }
-		
-		typedef LSBFirst Reverse;
-	};
-	
-	
-	struct High { public: enum { active = 0x01, inactive = 0x00 }; };
-	struct Low { public: enum { active = 0x00, inactive = 0x01 }; };
-	struct None { };
-	
-	struct InputFirst { public: enum { cpha = 0x00 }; };
-	struct OutputFirst { public: enum { cpha = 0x01 }; };
-
-	
-	static void begin()
+	static inline void begin()
 	{
 		pinMode(in, INPUT);
 		digitalWrite(in, HIGH);
@@ -57,35 +53,37 @@ public:
 		pinMode(out, OUTPUT);
 		digitalWrite(out, LOW);
 
-		pinMode(pinCLK, OUTPUT);
+		pinMode(clk, OUTPUT);
 		digitalWrite(pinCLK, LOW);
 	}
-	
-	static inline void bitOut(uint8_t b) { digitalWrite(out, b != 0? HIGH : LOW); }
-	static inline bool bitIn() { return digitalRead(in)? true : false; }
+
+	static inline void bitOut(uint8_t b)	{ digitalWrite(out, b != 0? HIGH : LOW); }
+	static inline bool bitIn()				{ return digitalRead(in); }
 };
+
+
 
 
 template <
 	typename BUS,
-	typename clkPol,		// アイドル時のクロック極性
+	uint8_t clkPol,			// アイドル時のクロック極性
 	typename clkPhase,		// 書き出し側のクロック極性
 	typename bitorder >
 class SerialDevice
 {
 protected:
-	inline SerialDevice() {}
-	inline ~SerialDevice() {}
+	inline SerialDevice()	{}
+	inline ~SerialDevice()	{}
 
 
 public:
-	static void begin() {}
-	static void end() {}
+	static inline void begin()		{}
+	static inline void end()		{}
 
-	static void pulse()
+	static inline void pulse()
 	{
-		digitalWrite(BUS::pinCLK, clkPol::inactive);
-		digitalWrite(BUS::pinCLK, clkPol::active);
+		digitalWrite(BUS::pinCLK, clkPol == LOW? HIGH : LOW);
+		digitalWrite(BUS::pinCLK, clkPol);
 	}
 
 	static void shift_out(uint8_t send, uint8_t bits)
@@ -99,7 +97,7 @@ public:
 			if (clkPhase::cpha != clkPol::active)
 				BUS::bitOut(send & bitorder::mask);
 				
-			digitalWrite(BUS::pinCLK, clkPol::active);
+			digitalWrite(BUS::pinCLK, clkPol);
 			send = bitorder::shift(send);
 		}
 	}
@@ -113,12 +111,12 @@ public:
 			if (clkPhase::cpha == 0x00 && BUS::bitIn())
 				recv |= bitorder::Reverse::mask;
 
-			digitalWrite(BUS::pinClk, clkPol::inactive);
+			digitalWrite(BUS::pinClk, clkPol == LOW? HIGH : LOW);
 
 			if (clkPhase::cpha != clkPol::active && BUS::bitIn())
 				recv |= bitorder::Reverse::mask;
 
-			digitalWrite(BUS::pinCLK, clkPol::active);
+			digitalWrite(BUS::pinCLK, clkPol);
 		}
 
 		return recv;
@@ -136,14 +134,14 @@ public:
 			else if (BUS::bitIn())
 				recv |= bitorder::Reverse::mask;
 
-			digitalWrite(BUS::pinClk, clkPol::inactive);
+			digitalWrite(BUS::pinClk, clkPol == LOW? HIGH : LOW);
 
 			if (clkPhase::cpha != clkPol::active)
 				BUS::bitOut(send & bitorder::mask);
 			else if (BUS::bitIn())
 				recv |= bitorder::Reverse::mask;
 				
-			digitalWrite(BUS::pinCLK, clkPol::active);
+			digitalWrite(BUS::pinCLK, clkPol);
 			send = bitorder::shift(send);
 		}
 		
@@ -171,11 +169,25 @@ public:
 	enum { pinClk = 13 };
 	enum { DIV2 = 0x04, DIV4 = 0x00, DIV8 = 0x05, DIV16 = 0x02, DIV32 = 0x06, DIV64 = 0x07, DIV128 = 0x03 };
 
-	static void begin();
+	static void begin()
+	{
+		super::begin();
+
+		pinMode(10, OUTPUT);
+		digitalWrite(10, HIGH);
+	
+		SPCR |= 1 << MSTR;
+	}
+
+
 	static inline void enable() { SPCR |= 1 << SPE; }
 	static inline void disable() { SPCR &= ~(1 << SPE); }
 
-	static void set_divider(int div);
+	static void set_divider(int div)
+	{
+		SPCR = (SPCR & ~SPI_CLOCK_MASK) | (div & SPI_CLOCK_MASK);
+		SPSR = (SPSR & ~SPI_2XCLOCK_MASK) | ((div >> 2) & SPI_2XCLOCK_MASK);
+	}
 	
 
 	template <typename clkPol, typename clkPhase>
@@ -194,19 +206,49 @@ public:
 
 
 	static void send(uint8_t value) { SPDR = value; }
-	static uint8_t recv();
+	static uint8_t recv()			{ send(0xff); return wait_for_complete(); }
 
-	static void wait_and_send(uint8_t value);
-	static uint8_t wait_and_recv();
+	static void wait_and_send(uint8_t value)	{ wait_for_complete(); send(value); }
+	static uint8_t wait_and_recv()				{ wait_for_complete(); return recv(); }
 
 	static bool is_running() { return (SPSR & (1 << SPIF)) == 0x00? true : false; }
-	static uint8_t wait_for_complete();
+	static uint8_t wait_for_complete()
+	{
+		while (is_running())
+ 			;
+	 	SPSR |= 1 << SPIF;
+	 	return SPDR;
+	}
 	
-	static void dummy_cycle(uint8_t n, uint8_t x);
-	static void drop_until(uint8_t x);
-	static void drop_until_with_timeout(uint8_t timeout, uint8_t x);
-	static uint8_t drop_while(uint8_t x);
-	static uint8_t drop_while_with_timeout(uint8_t timeout, uint8_t x);
+	static void dummy_cycle(uint8_t n, uint8_t x)
+	{
+		if (n == 0) return ;
+		send(x);
+		while (--n) wait_and_send(x);
+	}
+
+	static void drop_until(uint8_t x)
+	{
+		do { send(0xff); } while (wait_for_complete() != x) ;
+	}
+
+	static void drop_until_with_timeout(uint8_t timeout, uint8_t x) { while (timeout-- && recv() != x) ; }
+	static uint8_t drop_while(uint8_t x)
+	{
+		uint8_t r;
+		do { r = recv(); } while (r == x);
+		return r;
+	}
+
+	static uint8_t drop_while_with_timeout(uint8_t timeout, uint8_t x)
+	{
+		uint8_t r;
+		while (timeout--) {
+			r = recv();
+			if (r != x) break;
+		}
+		return r;
+	}
 };
 
 
@@ -217,18 +259,16 @@ template <
 	typename clkPol,		// アイドル時のクロック極性
 	typename clkPhase,		// 書き出し側のクロック極性
 	typename bitorder,
-	int clock_divider >
+	int clock_divider,
+	uint8_t ss>
 class SPIDevice
 {
 public:
 	inline SPIDevice() {}
 	inline ~SPIDevice() {}
 
-	int ss;
-
-	void begin(int _ss)
+	void begin()
 	{
-		this->ss = _ss;
 		pinMode(ss, OUTPUT);
 		disable();
 	}
@@ -241,10 +281,9 @@ public:
 		SPIMaster::enable();
 	}
 	inline void disable() { SPIMaster::disable(); }
-	
+
 	inline void select() { digitalWrite(this->ss, LOW); }
 	inline void deselect() { digitalWrite(this->ss, HIGH); }
-
 };
 
 
